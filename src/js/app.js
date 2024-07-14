b2fc706f016287ef2b27afc9330222edf4bdb826
dinar <saetonskie@mail.ru>
2024-07-14 14:54:38 +0300

    finish

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelector('.collapse_cont').classList.toggle('collapse_cont_second');
+    document.querySelector('.descr').classList.toggle('descr_second_state');
+
+
+});
